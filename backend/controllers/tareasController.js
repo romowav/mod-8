773459833eccc078b456c620707c47a504025400
Pablo Romo <djromo2296@gmail.com>
2024-01-30 20:22:58 -0600

    clase receso

--- backend/controllers/tareasController.js
+++ backend/controllers/tareasController.js
@@ -1,17 +1,23 @@
-const getTareas = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getTareas = asyncHandler( async (req, res) => {
   res.status(200).json({message: 'get tareas controller'})
-}
+})
 
-const postTareas = (req, res) => {
-  res.status(201).json({message: 'get tareas sip'})
-}
+const postTareas = asyncHandler( async (req, res) => {
+  if(!req.body.description){
+    res.status(400)
+    throw new Error('agrega una description CONTROLLER')
+  }
+  res.status(201).json({message: 'tarea creada!'})
+})
 
-const putTareas = (req, res) => {
+const putTareas = asyncHandler( async (req, res) => {
   res.status(200).json({message: `CONTROLER modifique la tarea con id ${req.params.id}`})
-}
+})
 
-const deleteTareas = (req, res) => {
+const deleteTareas = asyncHandler( async (req, res) => {
   res.status(200).json({message: ` CONTROLLER borre la tarea con id ${req.params.id}`})
-}
+})
 
 module.exports = { getTareas, postTareas, putTareas, deleteTareas }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const colors = require('colors')
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express()
+
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(errorHandler)
 
 app.use('/api/tareas', require('./routes/tareasRoutes'))
 
