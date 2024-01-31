cab90bb648ea1d90684c24a4c7e6ec8b8add9302
Pablo Romo <djromo2296@gmail.com>
2024-01-30 21:35:58 -0600

    errorhandler funciona, abajo de routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
-const errorHandler = require('./middleware/errorMiddleware')
+const errorHandler = require('./middleware/errorMiddleware.js')
 
 const app = express()
 
@@ -12,8 +12,8 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.use(errorHandler)
-
 app.use('/api/tareas', require('./routes/tareasRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`servidor inizializado en el puerto ${port}`))
